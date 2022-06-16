2b695f11ce68453d76b5b5be227a070f622f4fca
dhanaji33 <dhanajiwaghmode33@gmail.com>
2022-06-16 19:24:46 +0530

    implementing the game logic and manipulating the css when answer is correct

--- Guess-My-Number/starter/script.js
+++ Guess-My-Number/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 correct answer!';
+// document.querySelector('.number').textContent = '13';
+// document.querySelector('.score').textContent = '10';
+
+const SecretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = SecretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '☹ no number!';
+  } else if (guess === SecretNumber) {
+    document.querySelector('.message').textContent = '🎉 correct answer!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > SecretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You loose the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < SecretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You loose the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
