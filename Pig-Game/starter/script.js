c73cdb467ce68d9ebd3940a5a12c769ce806a002
dhanaji33 <dhanajiwaghmode33@gmail.com>
2022-06-20 14:10:52 +0530

    Swiching the active player functionality completed

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const currentScore0El = document.getElementById('current--0');
 const currentScore1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
@@ -9,11 +11,12 @@ const btnNewGame = document.querySelector('.btn--new');
 const btnHoldCore = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
-
 score1El.textContent = 0;
+
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 btnRollDice.addEventListener('click', function () {
   //
   diceEl.classList.remove('hidden');
@@ -22,7 +25,13 @@ btnRollDice.addEventListener('click', function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    currentScore0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
